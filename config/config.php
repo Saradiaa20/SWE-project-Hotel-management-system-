bb75cc9c65953957eef1ca548212d11669f448a9
ZiadMohamed123 <153671040+ZiadMohamed123@users.noreply.github.com>
2024-12-28 02:41:34 +0200

    Rearrange folder
    mvc

--- config/config.php
+++ config/config.php
@@ -3,7 +3,7 @@
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'room');
+define('DB_NAME', 'hotel_managment');
 
 // Create a connection to the database
 try {
